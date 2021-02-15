437b0afd1ea9c5cc8444616b508dae3a3574095b
Soponyai Áron <--global user.email aron.soponyai@katonaj-mkszig.hu>
2021-02-15 13:15:45 +0100

    házi meg cucc

--- Soponyai Áron/while1.py
+++ Soponyai Áron/while1.py
@@ -2,6 +2,6 @@ folytatja = True
 while folytatja:
     print("Csináld meg a leckét")
     valasz = input("Mondjam még egyszer? (i/n)")
-    if valasz = "n":
+    if valasz == "n":
         folytatja = False
 print("Na végre!") 
