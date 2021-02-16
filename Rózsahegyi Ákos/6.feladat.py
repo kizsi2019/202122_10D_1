1d34ab20b96e77f9ce40cb97166464394285f85e
Rozsahegyi Akos <akos.rozsahegyi@katonaj-mkszig.hu>
2021-02-16 14:06:55 +0100

    6. feladat (kijavított)

--- Rózsahegyi Ákos/6.feladat.py
+++ Rózsahegyi Ákos/6.feladat.py
@@ -1,3 +1,11 @@
-import random
-for randint in range (20):
-    print(random.randint(1,12))
+from random import randint
+
+szamlalo = 0
+i = 0
+while i < 20:
+    ertek = randint(1,12)
+    if ertek % 3 == 0:
+        print(ertek)
+        szamlalo +=1
+    i += 1
+print("Hárommal osztható szám ", szamlalo, " db volt.")
