4652121f5af109f83094ad648171683eeaf5aabe
Szabó Levenete <leventetamas.szabo@katonaj-mkszig.hu>
2021-03-08 17:41:23 +0100

    pótlás

--- Szabó Levente/páros4.py
+++ Szabó Levente/páros4.py
@@ -1,9 +1,9 @@
 folytatja = True
 while folytatja:
-    szám = int(input("Kérek egy páros számot"))
+    szám = int(input("Kérek egy páros számot! "))
     if szám%2 == 0:
         folytatja = False
     if not szám%2 == 0:
         folytatja = True
         print("Ez a szám nem páros. ")
-print("Ez egy páros szám! ")
+print("Ez a szám páros! ")
