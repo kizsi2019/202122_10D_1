38e5b4cd12a954af03706b5d1a98c9e4e3507c6c
K0CS1SB4L4Z5 <78422428+K0CS1SB4L4Z5@users.noreply.github.com>
2022-01-04 13:59:24 +0100

    Add files via upload

---
+++ Kocsis Balázs Tamás/f11.py
@@ -0,0 +1,39 @@
+import turtle
+def ablak_keszites(szin, ablaknev):
+    """
+    Egy ablak elkészítése, és a háttérszín, valamint az ablaknév
+beállítása.
+    Visszatérési érték: az új ablak.
+    """
+    a = turtle.Screen()
+    a.bgcolor(szin)
+    a.title(ablaknev)
+    return a
+
+
+def teknoc_keszites(szin, tm):
+    """
+    Létrehoz egy teknocöt, és beállítja az általa használt toll ˝
+színét és méretét.
+    Visszatérési érték: az új teknoc. ˝
+    """
+    t = turtle.Turtle()
+    t.color(szin)
+    t.pensize(tm)
+    return t
+
+
+a = ablak_keszites("lightgreen", "Eszti és Sanyi táncol")
+Eszti = teknoc_keszites("hotpink", 5)
+Sanyi = teknoc_keszites("black", 1)
+David = teknoc_keszites("yellow", 2)
+def csillag(hossz):
+    for i in range(5):
+        for i in range(5):
+            Sanyi.forward(100)
+            Sanyi.right(144)
+        Sanyi.forward(650)
+        Sanyi.right(144)
+
+csillag(20)
+a.mainloop()
---
+++ Kocsis Balázs Tamás/f2.py
@@ -0,0 +1,45 @@
+import turtle
+def ablak_keszites(szin, ablaknev):
+    """
+    Egy ablak elkészítése, és a háttérszín, valamint az ablaknév
+beállítása.
+    Visszatérési érték: az új ablak.
+    """
+    a = turtle.Screen()
+    a.bgcolor(szin)
+    a.title(ablaknev)
+    return a
+
+
+def teknoc_keszites(szin, tm):
+    """
+    Létrehoz egy teknocöt, és beállítja az általa használt toll ˝
+színét és méretét.
+    Visszatérési érték: az új teknoc. ˝
+    """
+    t = turtle.Turtle()
+    t.color(szin)
+    t.pensize(tm)
+    return t
+
+
+a = ablak_keszites("lightgreen", "Eszti és Sanyi táncol")
+Eszti = teknoc_keszites("hotpink", 5)
+Sanyi = teknoc_keszites("black", 1)
+David = teknoc_keszites("yellow", 2)
+def negyzet(sz):
+    for i in range(4):
+        Eszti.forward(sz)
+        Eszti.left(90)
+    Eszti.penup()
+    Eszti.backward(20)
+    Eszti.right(90)
+    Eszti.forward(20)
+    Eszti.left(90)
+    Eszti.pendown()
+meret = 20
+
+for i in range(5):
+    negyzet(meret)
+    meret = meret + 40
+a.mainloop()
