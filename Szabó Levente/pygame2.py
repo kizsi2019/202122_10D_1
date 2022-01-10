5cb02067bff1eea451cb0c9fc5051011deede328
Kinc470 <78422145+Kinc470@users.noreply.github.com>
2022-01-10 09:40:32 +0100

    pygame

---
+++ Szabó Levente/pygame2.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+GRAY = (127, 127, 127)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+screen = pygame.display.set_mode((600, 300))
+pygame.display.set_caption('Színváltó')
+background = GRAY
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+              running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                background = RED
+            elif event.key == pygame.K_g:
+                  background = GREEN
+
+    screen.fill(background)
+    pygame.display.update()
+
+pygame.quit()
