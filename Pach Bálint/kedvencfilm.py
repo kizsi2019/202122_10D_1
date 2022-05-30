893b88db74cb89e34dbad1274398e056d7b3a123
Balinzz <79915806+Balinzz@users.noreply.github.com>
2022-05-30 09:36:06 +0200

    Add files via upload

---
+++ Pach Bálint/kedvencfilm.py
@@ -0,0 +1,14 @@
+def oraperc(hossz):
+    ora = hossz // 60
+    perc = hossz - ora * 60
+    return str(ora) + "óra" + str(perc)+ "perc"
+        
+
+
+
+
+for _ in range(3):
+    film_nev = input("Add meg egy film címét! ")
+    hossz = int(input("Hány perces a film? "))
+    print("A(z)", film_nev, "című film", oraperc(hossz), "hosszú")
+    
