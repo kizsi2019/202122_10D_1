ba5a86b0b6393b92f3ced299c9711bc684775ff5
PeterTakacs34 <78421913+PeterTakacs34@users.noreply.github.com>
2022-05-31 14:15:36 +0200

    Add files via upload

---
+++ Takács Péter/Vizsga feladatok/Vizsga3.2.py
@@ -0,0 +1,19 @@
+import állat
+
+állatfajok = []
+
+for _ in range(3):
+    fajnév = input('Add meg egy állatfaj nevét! ')
+    tömeg = input('Hány kilogramm a tömege egy példánynak? ')
+    állatfaj = állat.Állatfaj(fajnév, tömeg)
+    állatfajok.append(állatfaj)
+    
+legnehezebb_állat = állatfajok[0]
+    
+for állatfaj in állatfajok:
+    print('A(z) ', állatfaj.fajnév, ' tömege ', állatfaj.tömeg, ' kg.', sep='')
+    if állatfaj.tömeg > legnehezebb_állat.tömeg:
+        legnehezebb_állat = állatfaj
+célfájl = open('legnehezebb.txt', 'w')
+print('A(z)', legnehezebb_állat.fajnév, 'a legnehezebb.', file=célfájl)
+célfájl.close()
