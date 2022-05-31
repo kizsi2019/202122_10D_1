4ac38eb37a9a806c305d93fdaede1df83375b0b6
kisshorvathbendeguz <90044269+kisshorvathbendeguz@users.noreply.github.com>
2022-05-31 13:55:25 +0200

    gyakorló feladatok

---
+++ Kiss-Horváth Bendegúz/python gyakorlas agazatira/allat.py
@@ -0,0 +1,4 @@
+class Állatfaj:
+    def __init__(self, fajnév, tömeg):
+        self.fajnév = fajnév
+        self.tömeg = tömeg
---
+++ Kiss-Horváth Bendegúz/python gyakorlas agazatira/nagyobb.py
@@ -0,0 +1,8 @@
+szam1 = int(input('Adj meg egy számot!'))
+szam2 = int(input('Adj meg egy másik számot!'))
+if szam1 > szam2:
+  print('Az első szám nagyobb.')
+elif szam2 > szam1:
+  print('A második szám nagyobb.')
+else:
+  print('A két szám egyenlő.')
