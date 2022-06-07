1530258aba075e24f86becd07bd308e05f978104
SimiRoli <roland.simon@katonaj-mkszig.hu>
2022-06-08 00:30:48 +0200

    update

--- Simon Roland/pythonfeladtsor/raketa.py
+++ Simon Roland/pythonfeladtsor/raketa.py
@@ -1,3 +1,9 @@
-start = int(input("Hány órás visszaszámlálást tervezünk?" ))
-for i in range(start;0; -1):
-    print("Visszaszámlálás: ", )
+start = int(input("Hány órás visszaszámlálást tervezünk?" )) 
+felfüggesztések = 0
+
+for szam in range(start,0, -1):
+    print('Visszaszámlálás:', szam)
+    válasz = input('Fel kell függeszteni a visszaszámlálást (i/n)? ')
+    if válasz == 'i':
+        felfüggesztések += 1
+print('A rakéta a visszaszámlálást követően ennyi órával indult:', start + felfüggesztések)
